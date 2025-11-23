c9fc409a867d4d37f26b353d63f0f018ee3450ff
Ekindurmus <2006ekindurmus@gmail.com>
2025-11-23 22:04:42 +0300

    Addition on dates.py

--- dates.py
+++ dates.py
@@ -1,4 +1,20 @@
-from datetime import datetime
+from datetime import datetime, timedelta 
 
+#today's date function
 current_date = datetime.now()
-print("Today is: " + str(current_date))
+print("Today is:" + str(current_date))
+
+#yesterday's date function
+one_day = timedelta(days=1)
+yesterday = current_date - one_day
+print("Yesterday was:" + str(yesterday))
+
+#ask user for a date input
+input_date = input("Please enter a date (DD/MM/YYYY):")
+input_date = datetime.strptime(input_date, "%d/%m/%Y")
+print("You entered: " + str(input_date))
+
+#calculate date one week from current date
+one_week = timedelta(days=7)
+week_after = input_date + one_week
+print("The date after one week is:" + str(week_after))
