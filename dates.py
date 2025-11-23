8b1be60b7d6b7d5b4bec3c6ef37d35e538428fd7
Ekindurmus <2006ekindurmus@gmail.com>
2025-11-23 22:37:50 +0300

    1 line is changed

--- dates.py
+++ dates.py
@@ -14,7 +14,7 @@ input_date = input("Please enter a date (DD/MM/YYYY):")
 input_date = datetime.strptime(input_date, "%d/%m/%Y")
 print("You entered: " + str(input_date))
 
-#calculate date one week from current date
-one_week = timedelta(days=7)
+#calculate date one week from current date input
+one_week = timedelta(weeks=1)
 week_after = input_date + one_week
 print("The date after one week is:" + str(week_after))
