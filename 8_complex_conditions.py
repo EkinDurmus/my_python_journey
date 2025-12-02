cb2bb19f0b02caf7c39074beb3d0f1df880e0d6d
Ekindurmus <2006ekindurmus@gmail.com>
2025-12-02 18:13:41 +0300

    Initial commit

---
+++ 8_complex_conditions.py
@@ -0,0 +1,39 @@
+#code challenge - my mission is:
+# When you join a hockey team you get your name on the back of the jersey
+# but the jersey may not be big enough to hold all the letters
+# Ask the user for their first name
+# Ask the user for their last name
+
+# if first name is < 10 characters and last name is < 10 characters 
+#       print first and last name on the jersey
+# if first name >= 10 characters long and last name is < 10 characters
+#       print first initial of first name and the entire last name
+# if first name < 10 characters long and last name is >= 10 characters
+#       print entire first name and first initial of last name
+# if first name >= 10 characters long and last name is >= 10 characters
+#       print last name only
+
+# Test with the following values
+# first name: Susan  last name: Ibach
+# output: Susan Ibach
+# first name: Susan  last name: ReallyLongLastName
+# output: Susan R.
+# first name: ReallyLongFirstName  last name: Ibach
+# output: R. Ibach
+# first name: ReallyLongFirstName  last name: ReallyLongLastName
+# output: ReallyLongLastName
+
+first_name = input ("Enter your first name: ").capitalize()
+last_name = input ("Enter your last name:").capitalize()
+
+if len(first_name)< 10 and len(last_name) < 10:
+    print(first_name + " " + last_name)
+elif len(first_name) >= 10 and len(last_name) < 10:
+    print(first_name[0] + ". " + last_name)
+elif len(first_name) < 10 and len(last_name) >= 10:
+    print(first_name + " " + last_name[0] + ".")
+elif len(first_name) >= 10 and len(last_name) >= 10:
+    print(last_name)
+else:
+    print("Unexpected input, please check the names entered.")
+ 
