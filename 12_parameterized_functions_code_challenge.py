70c5609853d77ed60b879b5d00f2a5091bfdc663
Ekindurmus <2006ekindurmus@gmail.com>
2025-12-10 20:25:39 +0300

    Added new content

---
+++ 12_parameterized_functions_code_challenge.py
@@ -0,0 +1,48 @@
+# Code Challenge - My mission is:
+# Create a calculator function
+# The function should accept three parameters:
+# first_number: a numeric value for the math operation
+# second_number: a numeric value for the math operation
+# operation: the word 'add' or 'subtract'. The default operation is 'add'
+# the function should return the result of the two numbers added or subtracted
+# based on the value passed in for the operator
+#
+# Test your function using named notation passing in numbers 6 and 4
+# Should return 10
+#
+# Test your function using named notation with the values 6,4, subtract 
+# Should return 2
+# 
+# BONUS: Test your function with the values 6, 4 and divide 
+# Have your function return an error message when invalid values are received
+
+def calculator(first_number, second_number, operation='add'):
+    if operation.upper() == 'ADD' or operation == '+':
+        return first_number + second_number
+    elif operation.upper() == 'SUBTRACT' or operation == '-':
+        return first_number - second_number
+    else:
+        return "Error: Invalid operation. Please use 'add' or 'subtract'."
+    
+first_number = input('Enter the first number: ')
+second_number = input('Enter the second number: ')
+
+default_calculation = calculator(first_number=float(first_number),\
+                       second_number=float(second_number))
+
+sub_calculation = calculator(first_number=float(first_number),\
+                     second_number=float(second_number),\
+                     operation='subtract')
+
+error_calculation = calculator(first_number=float(first_number),\
+                     second_number=float(second_number), 
+                     operation='divide')
+
+result = input('Which result do you want to see? (add/subtrack):')
+
+if result.upper() == 'ADD' or result == '+':
+    print('Addition result is:', default_calculation)
+elif result.upper() == 'SUBTRACT' or result == '-':
+    print('Subtraction result is:', sub_calculation)
+else:
+    print(error_calculation)
